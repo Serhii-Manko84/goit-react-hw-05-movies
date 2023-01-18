d6ee19c24557043d7aad8f9a77388e0623687477
Serhii Manko <msa01051984@gmail.com>
2023-01-18 17:09:18 +0200

    MovieDetails

--- src/components/pages/MovieDetails/MovieDetails.jsx
+++ src/components/pages/MovieDetails/MovieDetails.jsx
@@ -1,14 +1,20 @@
+import Loader from 'components/Loader/Loader';
+import PaheHeading from 'components/PageHeading/PageHeading';
 import React from 'react';
+
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { getMovieDetails } from 'services/api';
+
+import css from './MovieDetails.module.css';
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { movieID } = useParams();
+  const { movieId } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -16,22 +22,43 @@ const MovieDetails = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    const getDetailsMovies = async () => {
+    const fetchMovieDetails = async () => {
       try {
         setIsLoading(true);
-        const movies = await getMovieDetails();
-        setMovie(movies);
+        const movie = await getMovieDetails(movieId);
+
+        setMovie(movie);
       } catch (error) {
         setError('Something went wrong...');
-        console.log(error);
       } finally {
         setIsLoading(false);
       }
     };
-    getDetailsMovies();
-  }, [movieID]);
+    fetchMovieDetails();
+  }, [movieId]);
 
-  return <div>MovieDetails</div>;
+  return (
+    <>
+      <div>
+        <button type="text" onClick={handleClick} className={css.backButton}>
+          Go back
+        </button>
+      </div>
+      <div>{movie && <PaheHeading text={movie.title ?? movie.name} />}</div>
+      {isLoading && <Loader />}
+      {movie && (
+        <div className={css.movieDetails}>
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+            alt={movie.title ?? movie.name}
+          />
+          <h2>{movie.title ?? movie.name}</h2>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MovieDetails;
